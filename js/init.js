6afbdd66ba345a0259486e53d3790625d45d7d3e
LaloQuiroz <94099093+LaloQuiroz@users.noreply.github.com>
2021-11-15 19:51:35 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyChzw-m_UJa-xifoQR3ua6IXu1wamR81gE",
+  authDomain: "fbautentif.firebaseapp.com",
+  projectId: "fbautentif",
+  storageBucket: "fbautentif.appspot.com",
+  messagingSenderId: "804120316770",
+  appId: "1:804120316770:web:476a928fc07bc1164a3a3b",
+  measurementId: "G-NHZDXVKV5V"
+    });
